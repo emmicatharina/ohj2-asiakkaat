4ee205756cb1ec172c3666f5eb66decfd3c7ee77
emmicatharina <43492887+emmicatharina@users.noreply.github.com>
2020-12-10 22:21:58 +0200

    Korjattu olemattoman asiakkaan haku palauttamaan tyhjän objektin

--- AsiakkaatNeljas/src/control/Asiakkaat.java
+++ AsiakkaatNeljas/src/control/Asiakkaat.java
@@ -37,13 +37,17 @@ public class Asiakkaat extends HttpServlet {
 		} else if (pathInfo.indexOf("haeyksi") != -1) {
 			int asiakas_id = Integer.parseInt(pathInfo.replace("/haeyksi/", ""));
 			Asiakas asiakas = dao.etsiAsiakas(asiakas_id);
-			JSONObject JSON = new JSONObject();
-			JSON.put("asiakas_id", asiakas.getAsiakas_id());
-			JSON.put("etunimi", asiakas.getEtunimi());
-			JSON.put("sukunimi", asiakas.getSukunimi());
-			JSON.put("puhelin", asiakas.getPuhelin());
-			JSON.put("sposti", asiakas.getSposti());
-			strJSON = JSON.toString();
+			if (asiakas == null) {
+				strJSON = "{}";
+			} else {
+				JSONObject JSON = new JSONObject();
+				JSON.put("asiakas_id", asiakas.getAsiakas_id());
+				JSON.put("etunimi", asiakas.getEtunimi());
+				JSON.put("sukunimi", asiakas.getSukunimi());
+				JSON.put("puhelin", asiakas.getPuhelin());
+				JSON.put("sposti", asiakas.getSposti());
+				strJSON = JSON.toString();
+			}
 		} else {
 			String hakusana = pathInfo.replace("/", "");
 			asiakkaat = dao.listaaKaikki(hakusana);
